559f924b353c48b584f3cd0a23276f37ec813ea7
Matt Campbell <matt.dcamp@gmail.com>
2023-11-14 16:59:58 -0800

    First Test Action

--- jest.config.ts
+++ jest.config.ts
@@ -108,7 +108,7 @@ const config: Config = {
   // projects: undefined,
 
   // Use this configuration option to add custom reporters to Jest
-  // reporters: undefined,
+  // reporters:
 
   // Automatically reset mock state before every test
   // resetMocks: false,
--- test/unit/main.test.ts
+++ test/unit/main.test.ts
@@ -3,6 +3,6 @@ import { sum } from "../../src/main"
 describe('sum', () => {
     it('should sum numbers', () => {
         const actual = sum(1, 1);
-        expect(actual).toBe(2);
+        expect(actual).toBe(1);
     })
 })
