20f45109193795dbfae818f0a9da0ab55a769f8b
Matt Campbell <matt.dcamp@gmail.com>
2023-11-17 09:25:53 -0800

    Fix Root Folder Name and Path Algorithm

--- src/filesystem/folder.ts
+++ src/filesystem/folder.ts
@@ -1,33 +1,39 @@
 export interface Folder {
-  path: string;
+  name: string;
   parent: Folder | null;
   folders: Folder[];
   files: File[];
-}
-
-export function createRootFolder(): Folder {
-  return createFolder('/');
-}
-
-function createFolder(path: string, parent?: Folder | null): Folder {
-  return new FolderImpl(path, parent);
+  readonly path: string;
 }
 
 class FolderImpl implements Folder {
-  path: string;
+  name: string;
   parent: Folder | null;
   folders: Folder[];
   files: File[];
 
-  constructor(path: string, parent?: Folder | null) {
-    this.path = path;
+  constructor(name?: string, parent?: Folder | null) { 
+    this.name = name ?? '';
+
     if(parent === undefined) {
       this.parent = null;
     } else {
       this.parent=parent
     }
+
     this.folders = [];
     this.files = [];
   }
+
+  get path(): string {
+    if(this.parent === null) {
+      return '/'
+    } else {
+      return `${this.parent.path}${this.name}/`;
+    }
+  }
 }
 
+export const forTesting = {
+  FolderImpl
+};
