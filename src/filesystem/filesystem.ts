1b3022b5b1c83f1825d0d795e1a08b759ebe98ef
Matt Campbell <matt.dcamp@gmail.com>
2023-11-19 08:11:36 -0800

    Add Tests for cd

--- src/filesystem/filesystem.ts
+++ src/filesystem/filesystem.ts
@@ -2,7 +2,8 @@ import { type FolderDescriptor, FolderDescriptorImpl } from '../fileDescriptor';
 import { resolvePath } from './pathResolvers';
 
 export interface FileSystem {
-  cd: (path: string) => void;
+  cd: (path: string) => string;
+  pwd: () => string;
 }
 
 export function startFileSystem(): FileSystem {
@@ -18,12 +19,13 @@ export class FileSystemImpl implements FileSystem {
     this.workingFolder = this.rootFolder;
   }
 
-  cd(path: string): void {
+  cd(path: string): string {
     const newWorkingFolder = resolvePath(path, this.workingFolder, this.rootFolder);
     if (!newWorkingFolder.isFolder) {
       throw new Error(`Path ${path} is not a folder`);
     }
     this.workingFolder = newWorkingFolder as FolderDescriptor;
+    return this.pwd();
   }
 
   pwd(): string {
---
+++ src/filesystem/index.ts
@@ -0,0 +1,1 @@
+export { type FileSystem, startFileSystem } from './filesystem';
