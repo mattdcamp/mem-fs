c654d16265d42aeef11e1012f6c4953f1f96974e
Matt Campbell <matt.dcamp@gmail.com>
2023-11-24 11:45:22 -0800

    Add Example Operations to Main (#20)

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,25 @@
-console.log('hello world');
+import { startFileSystem } from './filesystem';
+
+const fs = startFileSystem();
+
+fs.mkdir('school', true);
+fs.cd('school');
+console.log(`get working directory => "${fs.pwd()}"`);
+
+fs.mkdir('homework', true);
+fs.cd('homework');
+fs.mkdir('math', true);
+fs.mkdir('lunch', true);
+fs.mkdir('history', true);
+fs.mkdir('spanish', true);
+fs.rm('lunch');
+console.log(`get working directory content => "${fs.ls()}"`);
+console.log(`get working directory => "${fs.pwd()}"`);
+
+fs.cd('..');
+fs.mkdir('cheatsheets', true);
+console.log(`get working directory content => "${fs.ls()}"`);
+fs.rm('cheatsheets');
+
+fs.cd('..');
+console.log(`get working directory => "${fs.pwd()}"`);
